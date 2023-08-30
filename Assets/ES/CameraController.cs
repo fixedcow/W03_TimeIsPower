df9c369448178ea59b903cf60b2c278a5563f963
김은서 <a55208806@gmail.com>
2023-08-30 17:22:04 +0900

    커믻

--- Assets/ES/CameraController.cs
+++ Assets/ES/CameraController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    [SerializeField] private float yOffset;
+    public float yOffset;
 
     private void Update()
     {
--- Assets/ES/PlayerGameOverManager.cs
+++ Assets/ES/PlayerGameOverManager.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerGameOverManager : MonoBehaviour
 {
     public bool isPlayerDaad;
 
     [SerializeField] private GameObject fadeScreen;
+    [SerializeField] private float fadeMiddlePositionX;
+    [SerializeField] private float fadeEndPositionX;
+    [SerializeField] private Vector3 fadeStartPosition;
+    [SerializeField] private float fadeTime;
     [SerializeField] private float restartInterval;
     [SerializeField] private Vector3 startPlayerPosition;
+    [SerializeField] private Vector3 startCameraPosition;
     [SerializeField] private GameObject recordTrigger;
+    [SerializeField] private GameObject cameraTrigger;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -23,12 +30,25 @@ public class PlayerGameOverManager : MonoBehaviour
     private void PlayerGameOver()
     {
         isPlayerDaad = true;
-        transform.position = startPlayerPosition;
+        //transform.position = startPlayerPosition;
+        fadeScreen.transform.DOLocalMoveX(fadeMiddlePositionX, fadeTime)
+             .OnComplete(() =>
+             {
+                 Camera.main.GetComponent<CameraController>().enabled = false;
+                 Camera.main.transform.position = startCameraPosition;
+                 transform.position = startPlayerPosition;
+                 fadeScreen.transform.DOLocalMoveX(fadeEndPositionX, fadeTime)
+                 .OnComplete(() =>
+                 {
+                     fadeScreen.transform.localPosition = fadeStartPosition;
+                 });
+             });
     }
 
     private void PlayerRestart()
     {
         recordTrigger.SetActive(true);
+        cameraTrigger.SetActive(true);
         isPlayerDaad = false;
     }
 }
