ee2b4fac60191600f5b83c0e2c6ae66e51a458c6
김은서 <a55208806@gmail.com>
2023-08-30 19:47:54 +0900

    커 밋

--- Assets/ES/GamePauseUI.cs
+++ Assets/ES/GamePauseUI.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GamePauseUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject pauseUI;
+    private PlayerAction input;
+    private bool isPause;
+
+   
+    private void Awake()
     {
+        input = new PlayerAction();
+    }
+    private void OnEnable()
+    {
+        input.Enable();
+        input.UI.ESC.performed += Pause;
+    }
+
+    private void OnDisable()
+    {
+        input.UI.ESC.performed -= Pause;
+        input.Disable();
+    }
+    private void PauseGameUI()
+    {
+        Time.timeScale = 0;
+        pauseUI.SetActive(true);
+    }
+
+    private void ResumeGameUI()
+    {
+        Time.timeScale = 1;
+        pauseUI.SetActive(false);
+    }
+
+    private void Pause(InputAction.CallbackContext _context)
+    {
+        Debug.Log("key");
+        if (isPause)
+        {
+            ResumeGameUI();
+            isPause = !isPause;
+        }
+        else
+        {
+            PauseGameUI();
+            isPause = !isPause;
+
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
