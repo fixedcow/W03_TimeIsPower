fbe41852ff44f477dc47db0a25c7d97ac9e9c163
fixedcow <puren37@gmail.com>
2023-08-31 13:21:57 +0900

    [Feat] : 튜토리얼룸 폴리싱
    공격버튼 안내 추가
    데스카운터 적용

--- Assets/JW/Scripts/Player.cs
+++ Assets/JW/Scripts/Player.cs
@@ -44,6 +44,7 @@ public class Player : MonoBehaviour
 		{
 			PlayerGameOver();
 			Invoke(nameof(PlayerRestart), restartInterval);
+			GameManager.instance.BattleEnd();
 			//GameManager.instance.GetBoss().Reset();
 		}
 	}
