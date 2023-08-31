42046d1929adf103764e281485a4a08d4b20623f
김은서 <a55208806@gmail.com>
2023-08-31 12:03:06 +0900

    마그마볼 스크립트 수정
    일정시간 지나면 사라지게 수정

--- Assets/ES/MagmaBall.cs
+++ Assets/ES/MagmaBall.cs
@@ -8,6 +8,8 @@ public class MagmaBall : MonoBehaviour
     [SerializeField] float guideSpeed;
     [SerializeField] float rotateSpeed;
     [SerializeField] float maxSpeed;
+    [SerializeField] float destroyMagmaTime;
+    private float timer;
     private Rigidbody2D rigid;
 
     private void Start()
@@ -16,18 +18,27 @@ public class MagmaBall : MonoBehaviour
     }
     private void GuideToPlayer()
     {
+        if (timer <= destroyMagmaTime)
+        {
+            
+            Vector2 dir = transform.right;
+            Vector2 targetDir = player.transform.position - transform.position;
+            Vector3 crossVec = Vector3.Cross(dir, targetDir);
+            float inner = Vector3.Dot(Vector3.forward, crossVec);
+            float saveAngle = inner + transform.rotation.eulerAngles.z;
+            transform.rotation = Quaternion.Euler(0, 0, saveAngle * rotateSpeed);
 
-        Vector2 dir = transform.right;
-        Vector2 targetDir = player.transform.position - transform.position;
-        Vector3 crossVec = Vector3.Cross(dir, targetDir);
-        float inner = Vector3.Dot(Vector3.forward, crossVec);
-        float saveAngle = inner + transform.rotation.eulerAngles.z;
-        transform.rotation = Quaternion.Euler(0, 0, saveAngle*rotateSpeed);
-
-        float moveDirAngle = transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
-        Vector2 moveDir = Vector2.zero;
-        moveDir = new Vector2(Mathf.Cos(moveDirAngle), Mathf.Sin(moveDirAngle));
-        rigid.velocity = moveDir*guideSpeed;
+            float moveDirAngle = transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
+            Vector2 moveDir = Vector2.zero;
+            moveDir = new Vector2(Mathf.Cos(moveDirAngle), Mathf.Sin(moveDirAngle));
+            rigid.velocity = moveDir * guideSpeed;
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        
 
     }
 
