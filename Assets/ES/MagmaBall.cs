47b9f50fd97568a4b6e2809c7cf1b497f2e4fe05
fixedcow <puren37@gmail.com>
2023-08-31 13:54:07 +0900

    [Fix] : 마그마볼, 파이어볼 버그 수정

--- Assets/ES/MagmaBall.cs
+++ Assets/ES/MagmaBall.cs
@@ -10,13 +10,13 @@ public class MagmaBall : MonoBehaviour
     [SerializeField] float maxSpeed;
     [SerializeField] float destroyMagmaTime;
     private float timer;
-    private Rigidbody2D rigid;
+    [SerializeField] private Rigidbody2D rigid;
 
-    private void Start()
-    {
-        rigid = GetComponent<Rigidbody2D>();
-    }
-    private void GuideToPlayer()
+	private void OnEnable()
+	{
+		player = GameManager.instance.GetPlayer().gameObject;
+	}
+	private void GuideToPlayer()
     {
         if (timer <= destroyMagmaTime)
         {
