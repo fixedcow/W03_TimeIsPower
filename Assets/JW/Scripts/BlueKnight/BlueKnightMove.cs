3affafc99dae891622372c8fad96faa0feae6b7a
김은서 <a55208806@gmail.com>
2023-09-03 22:54:43 +0900

    보스가 플레이어 따라오는 기능 추가

---
+++ Assets/JW/Scripts/BlueKnight/BlueKnightMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueKnightMove : MonoBehaviour
+{
+    [SerializeField] private bool canAct;
+    [SerializeField] private float moveSpeed;
+    public Transform testBoss;
+    public void CanAct() => canAct = true;
+
+    public void CanNotAct() => canAct = false;
+
+    private void FollowPlayer()
+    {
+        if (canAct)
+        {
+            float playerPosX = GameManager.instance.GetPlayer().transform.position.x;
+            float bossPosX = transform.position.x;
+            if (playerPosX > bossPosX)
+            {
+                transform.Translate(moveSpeed, 0, 0);
+            }
+            else
+            {
+                transform.Translate(-moveSpeed, 0, 0);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        FollowPlayer();
+    }
+}
