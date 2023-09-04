3018965148c368aaedc91606f2d6847f7146da66
ByeongHan Na <qudgks7946@gmail.com>
2023-09-04 21:50:54 +0900

    update scene flow 연결

--- Assets/BH/Scripts/TutorialEnterTrigger.cs
+++ Assets/BH/Scripts/TutorialEnterTrigger.cs
@@ -1,4 +1,6 @@
+using Cysharp.Threading.Tasks;
 using DG.Tweening;
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +11,7 @@ public class TutorialEnterTrigger : MonoBehaviour
     public Vector3 cameraPos;
     public GameObject gate;
     public GameObject fixedGate;
+    public BlackScreen black;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -23,11 +26,19 @@ public class TutorialEnterTrigger : MonoBehaviour
 
         if(collision.transform.position.x > 29)
         {
-            foreach(var go in TutorialManager.Instance.gates)
-            {
-                go.GetComponent<Gate>().KillTween();
-            }
-            SceneManager.LoadScene("Main");
+            //foreach(var go in TutorialManager.Instance.gates)
+            //{
+            //    go.GetComponent<Gate>().KillTween();
+            //}
+
+            FadeOut().Forget();
         }
+    }
+
+    [Button]
+    private async UniTaskVoid FadeOut()
+    {
+        await black.ScreenFadeOut();
+        SceneManager.LoadScene("Main");
     }
 }
