3e5f0b70896fb8ed1a847941cbe7961263f9df80
김은서 <a55208806@gmail.com>
2023-09-04 15:59:39 +0900

    도깨비불 패턴 구현중

---
+++ Assets/JW/Scripts/SoulTree/SoulTreeIllusionPattern.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulTreeIllusionPattern : BossPattern
+{
+    private GameObject illusion;
+    [SerializeField] private int illusionCount;
+    protected override void ActionContext()
+    {
+        
+    }
+}
